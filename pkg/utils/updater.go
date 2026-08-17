14ec20f0ec266f1d274fb3a309f9940410aa855d
ntlam20 <nguyenthanhlam1070@gmail.com>
2026-08-17 15:19:46 +0700

    Update

--- pkg/utils/updater.go
+++ pkg/utils/updater.go
@@ -58,7 +58,7 @@ func generateQuoteSVG(q Quote, date string) (string, error) {
 
 	const (
 		width    = 900
-		height   = 220
+		height   = 280
 		cx       = width / 2
 		lineH    = 32
 		maxChars = 52
